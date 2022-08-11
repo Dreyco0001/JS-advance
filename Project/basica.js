f6cfe02855db5092c79020e0bcb5a6ffff84f464
Dreyco0001 <dreycoarak00@gmail.com>
2022-08-10 21:09:45 -0400

    avance de la clase

--- Project/basica.js
+++ Project/basica.js
@@ -1,4 +1,18 @@
-console.log (hola)
-setTimeout(() => {
-   console.log(tito) 
-}, 5000);
+// console.log (hola)
+
+// setTimeout(() => {
+//    console.log(tito) 
+// }, 5000);
+
+
+
+
+
+
+// anonimas
+
+// trasformar a una IIFE
+
+(function (a,b) {return console.log(a+b)})(25, 27);
+
+((a , b) => console.log(a + b)) (5,89);
