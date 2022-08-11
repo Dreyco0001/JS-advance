076576b2db4f6729d20252c52e893017faf1a221
Dreyco0001 <dreycoarak00@gmail.com>
2022-08-10 20:24:43 -0400

    avance

---
+++ Project/Asincrono.js
@@ -0,0 +1,19 @@
+// sincrono
+
+// console.log("1")
+// console.log("2")
+// console.log("3")
+
+
+
+// asincrono
+
+console.log("1")
+
+setTimeout(() => {
+    console.log("2")
+}, 3000);
+
+console.log("3")
+
+
